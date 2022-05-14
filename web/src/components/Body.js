7cc31576a4dc48964bb40f507fb09ccfacf2ec61
jordan <jordan@bitola.com>
2022-05-14 00:43:51 -0400

    Token names + token addresses accordion

--- web/src/components/Body.js
+++ web/src/components/Body.js
@@ -1,28 +1,39 @@
 // SPDX-License-Identifier: BUSL-1.1
 import React from 'react';
-import { Form, Card } from 'react-bootstrap';
+import { Form, Accordion } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'; 
+import { ethers } from 'ethers';
 import OrderBook from './OrderBook';
+import afIERC20 from '../@artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json';
 
 function Body({provider}) {
     const [t1Address, setT1Address] = React.useState("");
     const [t2Address, setT2Address] = React.useState("");
+    const [t1Symbol, setT1Symbol] = React.useState("");
+    const [t2Symbol, setT2Symbol] = React.useState("");
 
-    const onT1AddressChange = (e) => {
+    const onT1AddressChange = async (e) => {
         setT1Address(e.currentTarget.value);
+        const signer = provider.getSigner();
+        const token1 = new ethers.Contract(e.currentTarget.value, afIERC20.abi, signer);
+        setT1Symbol(await token1.symbol());
     }
 
-    const onT2AddressChange = (e) => {
+    const onT2AddressChange = async (e) => {
         setT2Address(e.currentTarget.value);
+        const signer = provider.getSigner();
+        const token2 = new ethers.Contract(e.currentTarget.value, afIERC20.abi, signer);
+        setT2Symbol(await token2.symbol());
     }
 
     return (<>
-        <Card>
-            <Card.Header>Token Pair</Card.Header>
-            <Card.Body>
+        <Accordion>
+            <Accordion.Item eventKey="0">
+            <Accordion.Header>{t2Symbol} / {t1Symbol}</Accordion.Header>
+            <Accordion.Body>
                 <Form>
                     <Form.Group className="mb-3" controlId="formT1">
-                        <Form.Label>Token1 address</Form.Label>
+                        <Form.Label>Token1 ({t1Symbol}) address</Form.Label>
                         <Form.Control onChange={onT1AddressChange} />
                         <Form.Text className="text-muted">
                         Enter the address of the token contract.
@@ -30,17 +41,18 @@ function Body({provider}) {
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formT2">
-                        <Form.Label>Token2 address</Form.Label>
+                        <Form.Label>Token2 ({t2Symbol}) address</Form.Label>
                         <Form.Control  onChange={onT2AddressChange} />
                         <Form.Text className="text-muted">
                         Enter the address of the token contract.
                         </Form.Text>
                     </Form.Group>
                 </Form>
-            </Card.Body>
-        </Card>
+            </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
         <OrderBook t1Address={t1Address} t2Address={t2Address} provider={provider} />
-        </>);
+    </>);
 }
 
 export default Body;
