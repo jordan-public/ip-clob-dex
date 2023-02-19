9549d078bba798924bd4b7f3bb17e10066b9349b
jordan <jordan@bitola.com>
2023-02-18 21:16:24 -0500

    Wallet fixes

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,11 +1,16 @@
  
 const webpack = require('webpack'); 
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 module.exports = function override(config) { 
    config.plugins = (config.plugins || []).concat([ 
    	new webpack.ProvidePlugin({ 
     	process: 'process/browser', 
       Buffer: ['buffer', 'Buffer'] 
-    }) 
+    }),
+    new NodePolyfillPlugin({
+      excludeAliases: ['console'],
+    })
    ]) 
    return config; 
 }
--- web/src/components/Account.js
+++ web/src/components/Account.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { ethers } from "ethers";
 import Web3Modal from "web3modal";
-import WalletConnectProvider from '@walletconnect/ethereum-provider';
+import WalletConnectProvider from '@walletconnect/web3-provider';
 
 function Account({provider, setProvider, address, setAddress}) {
     const [network, setNetwork] = React.useState(null);
@@ -11,28 +11,31 @@ function Account({provider, setProvider, address, setAddress}) {
 
     React.useEffect(() => {
         if (!window.ethereum) return;
-        window.ethereum.on("connect", info => console.log("Connected: ", info) );
-        window.ethereum.on('accountsChanged', accounts => { 
+        const onConnect = info => console.log("Connected: ", info);
+        window.ethereum.on("connect",  onConnect);
+        const onAaccountsChanged = accounts => { 
             if (accounts && accounts.length > 0)
                 setAddress(accounts[0].toLowerCase())
             else
                 setAddress(null);
-        });
-        window.ethereum.on('chainChanged', chainId => window.location.reload() );
-        window.ethereum.on('disconnect', async (error: { code: number; message: string }) => {
+        };
+        window.ethereum.on('accountsChanged', onAaccountsChanged);
+        const onChainChanged = chainId => window.location.reload();
+        window.ethereum.on('chainChanged', onChainChanged);
+        const onDisconnect = async (error: { code: number; message: string }) => {
             console.log("Disconnected: ", error);
             setNetwork(null);
             setAddress(null);
             setProvider(null);
             if (web3Modal) await web3Modal.clearCachedProvider();
-        });
-        // return () => {
-        //     // Add references to original functions, not re-writes
-        //     window.ethereum.removeListener("connect", info => console.log(info) );
-        //     window.ethereum.removeListener('accountsChanged', accounts => setAddress(accounts[0].toLowerCase()) );
-        //     window.ethereum.removeListener('chainChanged', chainId => window.location.reload() );
-        //     window.ethereum.removeListener('disconnect', (error: { code: number; message: string }) => console.log(error) );
-        //     }
+        };
+        window.ethereum.on('disconnect', onDisconnect);
+        return () => {
+            window.ethereum.removeListener("connect", onConnect);
+            window.ethereum.removeListener('accountsChanged', onAaccountsChanged );
+            window.ethereum.removeListener('chainChanged', onChainChanged );
+            window.ethereum.removeListener('disconnect', onDisconnect);
+        }
     }, []);
 
     const onConnect = async () => {
@@ -44,10 +47,12 @@ function Account({provider, setProvider, address, setAddress}) {
             const walletConnectEthereum = {
               package: WalletConnectProvider,
               options: {
-                bridge: process.env.ROPSTEN_BRIDGE,
-                //bridge: 'http://127.0.0.1:8545/',
-                infuraId: process.env.ROPSTEN_INFURA_ID,
-                rpc: {},
+                rpc: {
+                    5: "http://127.0.0.1:8545/",
+                    137: "https://rpc-mainnet.matic.network",
+                    31337: "http://127.0.0.1:8545",
+                    80001: "https://matic-mumbai.chainstacklabs.com",
+                },
               },
             };
             providerOptions.walletconnect = walletConnectEthereum;
